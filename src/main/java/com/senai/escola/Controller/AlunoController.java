af15bece41c39dfa58879e148745f3c7938ca411
LucasGrem <lucasgrem07@gmail.com>
2025-08-13 21:51:25 -0300

    feat: class controller

--- src/main/java/com/senai/escola/Controller/AlunoController.java
+++ src/main/java/com/senai/escola/Controller/AlunoController.java
@@ -1,4 +1,38 @@
 package com.senai.escola.Controller;
 
+import com.senai.escola.Models.Aluno;
+import com.senai.escola.Service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //defini que esta class vire a controladora
+@RequestMapping ("/alunos") //Faz integração com a web
 public class AlunoController {
+    private final AlunoService alunoService; //precisa de um construtor, para que a variavel seja inicializada.
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<Aluno> buscarAlunos(){
+        return alunoService.buscarTodosAlunos();
+    }
+
+    @PostMapping
+    public Aluno salvar(@RequestBody Aluno aluno){
+        return alunoService.salvarNovoAluno(aluno);
+    }
+
+    @DeleteMapping
+    public void excluirAluno(Long id){ //"void" significa q é sem retorno
+        alunoService.deletarAluno(id);
+    }
+
+    @GetMapping("/{id}") //"("/{id}")" é para buscar algo especifico
+    public Aluno buscarAlunoPorId(@PathVariable Long id){ //O PathVariable
+        return alunoService.buscarAlunoId(id);
+    }
+
 }
