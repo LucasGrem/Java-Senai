47ca9ebdcf5c9140b4133d87071c1e9642288fd0
LucasGrem <lucasgrem07@gmail.com>
2025-08-14 21:51:34 -0300

    feat: java 14-08-2025

--- src/main/java/com/senai/escola/Controller/AlunoController.java
+++ src/main/java/com/senai/escola/Controller/AlunoController.java
@@ -1,13 +1,20 @@
 package com.senai.escola.Controller;
 
-import com.senai.escola.Models.Aluno;
-import com.senai.escola.Service.AlunoService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.escola.Models.Aluno;
+import com.senai.escola.Service.AlunoService;
+
 @RestController //defini que esta class vire a controladora
-@RequestMapping ("/alunos") //Faz integração com a web
+@RequestMapping ("/{alunos}") //Faz integração com a web
 public class AlunoController {
     private final AlunoService alunoService; //precisa de um construtor, para que a variavel seja inicializada.
 
@@ -25,14 +32,16 @@ public class AlunoController {
         return alunoService.salvarNovoAluno(aluno);
     }
 
-    @DeleteMapping
-    public void excluirAluno(Long id){ //"void" significa q é sem retorno
+    @DeleteMapping("/{id}")
+    public void excluirAluno(@PathVariable Long id){ //"void" significa q é sem retorno
         alunoService.deletarAluno(id);
     }
 
     @GetMapping("/{id}") //"("/{id}")" é para buscar algo especifico
-    public Aluno buscarAlunoPorId(@PathVariable Long id){ //O PathVariable
+    public Aluno buscarAlunoPorId(@PathVariable Long id){
         return alunoService.buscarAlunoId(id);
     }
+
+
 
 }
